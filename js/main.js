1f1f92aefa884feda8a9be4b9c06207536f0a9b8
artisanint <lg.mphuthi1@gmail.com>
2023-03-31 04:45:59 +0200

    footer completed and bug fixes

--- js/main.js
+++ js/main.js
@@ -8,16 +8,18 @@ const mobile_btn = document.querySelector('.mobile_btn');
 
 
 let tl = gsap.timeline({defaults:{reversed: true}});
-let tl2 = gsap.timeline({defaults:{reversed: true}});
+let tl2 = gsap.timeline({defaults:{reversed: true}, paused: true});
 
 tl.to('.drop_down',{opacity: 1,height:"200px",duration: 0.5});
 tl2.to('nav',{y:0,duration: 0.5});
 
 drop_down_btn.addEventListener("click", function (){
     tl.reversed() ? tl.play() : tl.reverse();
+    
   })
   
 mobile_btn.addEventListener("click", function (){
     tl2.reversed() ? tl2.play() : tl2.reverse();
+    
   })
   
